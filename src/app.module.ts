1e3d82c221073d50afa56eb8c1a036467c3e5546
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-03-31 08:31:59 +0200

    Feat: basculer envoi de mail sur port sécurisé :sparkles: :lock: (#20)

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,7 +33,7 @@ import * as path from "path";
       transport: {
         host: `${process.env.MAIL_HOST}`,
         port: `${process.env.MAIL_PORT}`,
-        secure: false, // true for 465, false for other ports
+        secure: true, // true for 465, false for other ports
         auth: {
           user: `${process.env.MAIL_USER}`,
           pass: `${process.env.MAIL_PASSWORD}`
