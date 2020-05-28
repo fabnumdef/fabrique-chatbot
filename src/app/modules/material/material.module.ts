080695c6cdb459f5f9dd6ab1b2a90acbccf31469
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-05-28 13:23:14 +0200

    Feat: changement images landing :sparkles: :art:

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -14,18 +14,19 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCarouselModule } from '@ngmodule/material-carousel';
 
 @NgModule({
   declarations: [],
   imports: [
     MatStepperModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule, MatDividerModule,
     MatSelectModule, MatRadioModule, MatDialogModule, MatTableModule, MatProgressBarModule, MatTooltipModule, MatMenuModule,
-    MatCheckboxModule
+    MatCheckboxModule, MatCarouselModule.forRoot()
   ],
   exports: [
     MatStepperModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule, MatDividerModule,
     MatSelectModule, MatRadioModule, MatDialogModule, MatTableModule, MatProgressBarModule, MatTooltipModule, MatMenuModule,
-    MatCheckboxModule
+    MatCheckboxModule, MatCarouselModule
   ]
 })
 export class MaterialModule { }
