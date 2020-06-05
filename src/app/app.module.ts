29185d4323737490ba4f01eb7c419e2b1d1c2d86
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-06-05 06:23:40 +0200

    Feat: accessibility, best practice, pwa :sparkles:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { environment } from '../environments/environment';
     BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: true })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
