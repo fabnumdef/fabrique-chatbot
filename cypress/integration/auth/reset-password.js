97b6c76b11b4d13b41a51ad11a83425df55ac7e3
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-10-16 16:16:56 +0200

    Feat: update tests e2e :lock:

--- cypress/integration/auth/reset-password.js
+++ cypress/integration/auth/reset-password.js
@@ -12,25 +12,48 @@ describe('Testing the Reset Password page', function() {
   it('should disable reset password btn if the form is not valid', function() {
     cy.get('[data-cy=ResetPasswordBtn]').should('be.disabled');
 
-    cy.get('[data-cy=PasswordInput]').type('password');
+    cy.get('[data-cy=PasswordInput]').type('passwordA0');
     cy.get('[data-cy=ResetPasswordBtn]').should('be.disabled');
 
     cy.get('[data-cy=PasswordInput]').clear();
-    cy.get('[data-cy=PasswordCheckInput]').type('password');
+    cy.get('[data-cy=PasswordCheckInput]').type('passwordA0');
     cy.get('[data-cy=ResetPasswordBtn]').should('be.disabled');
-
 
     cy.get('[data-cy=PasswordInput]').clear();
     cy.get('[data-cy=PasswordCheckInput]').clear();
     cy.get('[data-cy=PasswordInput]').type('password');
-    cy.get('[data-cy=PasswordCheckInput]').type('passwordBis');
+    cy.get('[data-cy=PasswordCheckInput]').type('password');
+    cy.get('[data-cy=ResetPasswordBtn]').should('be.disabled');
+
+    cy.get('[data-cy=PasswordInput]').clear();
+    cy.get('[data-cy=PasswordCheckInput]').clear();
+    cy.get('[data-cy=PasswordInput]').type('passwordA');
+    cy.get('[data-cy=PasswordCheckInput]').type('passwordA');
+    cy.get('[data-cy=ResetPasswordBtn]').should('be.disabled');
+
+    cy.get('[data-cy=PasswordInput]').clear();
+    cy.get('[data-cy=PasswordCheckInput]').clear();
+    cy.get('[data-cy=PasswordInput]').type('password0');
+    cy.get('[data-cy=PasswordCheckInput]').type('password0');
+    cy.get('[data-cy=ResetPasswordBtn]').should('be.disabled');
+
+    cy.get('[data-cy=PasswordInput]').clear();
+    cy.get('[data-cy=PasswordCheckInput]').clear();
+    cy.get('[data-cy=PasswordInput]').type('Pass0');
+    cy.get('[data-cy=PasswordCheckInput]').type('Pass0');
+    cy.get('[data-cy=ResetPasswordBtn]').should('be.disabled');
+
+    cy.get('[data-cy=PasswordInput]').clear();
+    cy.get('[data-cy=PasswordCheckInput]').clear();
+    cy.get('[data-cy=PasswordInput]').type('passwordA0');
+    cy.get('[data-cy=PasswordCheckInput]').type('passwordA0Bis');
     cy.get('[data-cy=ResetPasswordBtn]').should('be.disabled');
     cy.get('.mat-error').should('contain', 'Les mot de passe ne correspondent pas.');
   });
 
   it('should enable button if passwords are same', function() {
-    cy.get('[data-cy=PasswordInput]').type('password');
-    cy.get('[data-cy=PasswordCheckInput]').type('password');
+    cy.get('[data-cy=PasswordInput]').type('passwordA0');
+    cy.get('[data-cy=PasswordCheckInput]').type('passwordA0');
     cy.get('[data-cy=ResetPasswordBtn]').should('be.enabled');
   });
 });
