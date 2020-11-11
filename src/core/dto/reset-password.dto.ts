8bcfc66319a8e843e3495475c82b13690b7f55fc
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-11-11 10:03:39 +0100

    Feat: password special chars :sparkles:

--- src/core/dto/reset-password.dto.ts
+++ src/core/dto/reset-password.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString, Matches } from "class-validator";
 export class ResetPasswordDto {
   @IsString()
   @IsNotEmpty()
-  @Matches(/^(?=\D*\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,200}$/gm)
+  @Matches(/^(?=\D*\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=.*[@$!%*?&]).{8,200}$/gm)
   password: string;
 
   @IsString()
